91cbd8dd9efad88eef64871bf1a0d5a2e5f18ef3
Haruya Kanamitsu <hkanamit@c4r2s16.42tokyo.jp>
2026-06-17 21:37:06 +0900

    made ex0

---
+++ ex0/construct.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import site
+
+
+def main() -> None:
+    print("\nMATRIX STATUS: ", end="")
+    in_venv: bool = False
+    if sys.base_prefix == sys.prefix:
+        print("You're still plugged in\n")
+    else:
+        print("Welcome to the construct\n")
+        in_venv = True
+    print(f"Current Python: {sys.executable}")
+    print("Virtual Environment: ", end="")
+    if not in_venv:
+        print(" None detected\n")
+        print(
+            "WARNING: You're in the global environment!\n"
+            "The machines can see everything you install.\n"
+            )
+        print(
+            "To enter the construct, run:\n"
+            "python -m venv matrix_env\n"
+            "source matrix_env/bin/activate # On Unix\n"
+            "matrix_env\\Scripts\\activate # On Windows\n")
+        print("Then run this program again.")
+    else:
+        venv_name = os.path.basename(sys.prefix)
+        print(venv_name)
+        print(f"Environment Path: {sys.prefix}\n")
+        print(
+            "SUCCESS: You're in an isolated environment!\n"
+            "Safe to install packages without affecting\n"
+            "the global system.\n"
+            )
+        print(
+            "Package installation path: \n"
+            f"{site.getusersitepackages()}"
+            )
+
+
+if __name__ == "__main__":
+    main()
